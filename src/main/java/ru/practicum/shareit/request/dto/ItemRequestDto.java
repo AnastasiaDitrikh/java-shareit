2ccb89d7781cfdd232c89a0f9b730ffa7065b216
DitrikhAnastasia <ditrihanastasya@yandex.ru>
2023-07-25 00:26:02 +0400

    Реализация проекта в рамках ТЗ15.correct codestyle

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.shareit.item.dto.ItemDtoOut;
 
 import javax.validation.constraints.NotNull;
